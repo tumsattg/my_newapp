f2d7150b1c8549e548bea9fbe1e69bae7cf2ae36
tumsattg <121270339+tumsattg@users.noreply.github.com>
2023-01-15 11:57:51 +0000

    1st

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import streamlit as st
+from scipy.optimize import minimize
 
-st.title("タイトル")
-st.write("本文")
+def optimize():
+    st.title("Optimization Demo")
+    algorithm = st.selectbox("Select optimization algorithm", ["BFGS", "L-BFGS-B", "SLSQP"])
+    func = st.text_input("Enter a function to optimize")
+    x0 = st.number_input("Enter initial value for x")
+    res = minimize(lambda x: eval(func), x0, method=algorithm)
+    st.write("Optimized value: ", res.x)
+
+if __name__ == "__main__":
+    optimize()
